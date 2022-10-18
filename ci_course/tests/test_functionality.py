a5c0a39dbf2ca35ee896087957c2b610985091b8
Ruby Nixson <ruby.nixson@maths.ox.ac.uk>
2022-10-18 17:08:29 +0100

    Added unit test for missing line

--- ci_course/tests/test_functionality.py
+++ ci_course/tests/test_functionality.py
@@ -13,6 +13,7 @@ class TestFunctionality(unittest.TestCase):
         self.assertEqual(ci_course.minimum(1, 2, 3), 1)
         self.assertEqual(ci_course.minimum(1.2, 2.3), 1.2)
         self.assertEqual(ci_course.minimum(-1.2, -3), -3)
+        self.assertEqual(ci_course.minimum("Hello", "not", "number"), None)
 
 
 if __name__ == '__main__':
